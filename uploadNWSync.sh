74bb047d77f6be02105e5a0aeac6c0011523fb49
Stefano Dobbin <45202623+StefanoND@users.noreply.github.com>
2023-07-25 09:04:20 +0000

    Update uploadNWSync.sh

--- uploadNWSync.sh
+++ uploadNWSync.sh
@@ -1,2 +1,5 @@
 docker buildx build . -f nwsync.dockerfile -t nwnci/nwsync
 docker run --rm -it --env-file ./env.list nwnci/nwsync:latest
+if [[ -f nwsync/README.md ]]; then
+    rm nwsync/README.md
+fi
